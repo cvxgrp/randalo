1928b13d5490fc55cb158175804d9ba2c93bad58
Parth Nobel <parthnobel@berkeley.edu>
2023-11-15 00:30:50 -0800

    Fixes line search prox grad

--- alogcv/prox_grad.py
+++ alogcv/prox_grad.py
@@ -14,8 +14,10 @@ def _line_search(x, lambda_init, f, grad_f, prox_g):
     f_x = f(x)
     while True:
         z = prox_g(x - lambda_ * grad_fx, lambda_)
-        norm_z_m_x = torch.linalg.vector_norm(z - x)
-        if f(z) <= f_x + grad_fx @ (z - x) + 1 / 2 / lambda_ * norm_z_m_x**2:
+        zmx = z - x
+        norm_z_m_x = torch.linalg.vector_norm(zmx)
+        if f(z) <= f_x + grad_fx @ zmx + 1 / 2 / lambda_ * norm_z_m_x**2:
+            err = torch.linalg.vector_norm(grad_fx + 1 / lambda_ * (zmx))
             break
         lambda_ = beta * lambda_
 
@@ -27,19 +29,21 @@ def prox_grad_w_linesearch(
     grad_f: Callable[[torch.Tensor], torch.Tensor],
     prox_g: Callable[[torch.Tensor], torch.Tensor],
     x0: torch.Tensor,
-    _,
+    lipschitz_constant: torch.Tensor,
     max_iters=5000,
     eps=1e-3,
 ) -> tuple[torch.Tensor, int]:
     x = x0
     norm_delta_x = float("inf")
-    lambda_ = 1.0
+    lambda_ = 0.8 / lipschitz_constant
     k = 0
     epsilon = eps * math.sqrt(x0.numel())
 
-    while norm_delta_x > epsilon and k < max_iters:
-        lambda_, x, norm_delta_x = _line_search(x, lambda_, f, grad_f, prox_g)
+    while err > epsilon and k < max_iters:
+        xprev = x
+        lambda_, x, err = _line_search(x, lambda_, f, grad_f, prox_g)
         k += 1
+        #print(f'{k=} {lambda_=} {err=}')
 
     return x, k
 
@@ -49,23 +53,23 @@ def accel_prox_grad_w_linesearch(
     grad_f: Callable[[torch.Tensor], torch.Tensor],
     prox_g: Callable[[torch.Tensor], torch.Tensor],
     x0: torch.Tensor,
-    _,
+    lipschitz_constant: torch.Tensor,
     max_iters=5000,
-    eps=1e-3,
+    eps=1e-5,
 ) -> tuple[torch.Tensor, int]:
     xprev = 0
     x = x0
-    norm_delta_x = float("inf")
-    lambda_ = 1.0
+    err = float("inf")
+    lambda_ = 0.8 / lipschitz_constant
     k = 0
     epsilon = eps * math.sqrt(x0.numel())
 
-    while norm_delta_x > epsilon and k < max_iters:
+    while err > epsilon and k < max_iters:
         y = x + k / (k + 3) * (x - xprev)
         xprev = x
-        lambda_, x, _ = _line_search(y, lambda_, f, grad_f, prox_g)
-        norm_delta_x = torch.linalg.vector_norm(x - xprev).item()
+        lambda_, x, err = _line_search(y, lambda_, f, grad_f, prox_g)
         k += 1
+        #print(f'{k=} {lambda_=} {err=}')
 
     return x, k
 
@@ -122,6 +126,6 @@ def accel_prox_grad(
         x = prox_g(intermediate_y, lambda_)
         k += 1
         err = torch.linalg.vector_norm(grad_f(x) - 1 / lambda_ * (x - intermediate_y))
-        # print(f"{k=}, {err=}, {lambda_=}")
+        #print(f"{k=}, {err=}, {lambda_=}")
 
     return x, k
