84aad6ef5e28289562c928436e548e3b39e717d8
Daniel LeJeune <16905520+dlej@users.noreply.github.com>
2023-11-30 18:42:28 -0800

    fix ratios and add equicorrelated

--- benchmarking/generate_problems.py
+++ benchmarking/generate_problems.py
@@ -1,4 +1,4 @@
-import np
+import numpy as np
 
 min_n_or_ps = [100, 1_000, 10_000, 100_000]
 ratios_for_n_over_s = [5, 4, 3, 2, 1, 0.8, 0.6, 0.4, 0.2]
@@ -11,12 +11,12 @@ for sparse in sparsity_solution:
         for ratio in ratios_for_n_over_s:
             if ratio <= 1:
                 n = min_n_or_p
-                s = ratio * n
+                s = int(n / ratio)
                 p = 5 * s if sparse else s
             else:
-                p = min_n_or_p
-                n = ratio * p
-                s = p // 5 if sparse else p
+                s = min_n_or_p
+                n = ratio * s
+                p = 5 * s if sparse else s
             beta = np.zeros(p)
             beta[:s] = 1 / np.sqrt(s)
             for i, distribution in enumerate(distributions):
@@ -32,8 +32,10 @@ for sparse in sparsity_solution:
                         X = ...
                         continue
                     case "equicorrelated":
-                        X = ...
-                        continue
+                        x0 = rng.normal(size=n)
+                        a = np.sqrt(0.5)
+                        b = np.sqrt(1 - a**2)
+                        X = a * rng.normal(size=(n, p)) + b * x0[:, None]
                     case "sparse":
                         X = ...
                         continue
