8b3a245b9323d8704f80adbd8a772f584a8ca611
Daniel LeJeune <16905520+dlej@users.noreply.github.com>
2024-04-08 11:59:09 -0700

    BKS convergence exp

---
+++ benchmarking/lasso_bks_convergence.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --job-name=lasso_bks_convergence
+#SBATCH --output=lasso_bks_convergence/output/slurm-%A_%a.out
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=16GB
+#SBATCH --partition=candes
+#SBATCH --array=1-300
+
+BASE_DIR=$HOME/alo/benchmarking/lasso_bks_convergence
+CONFIGS_DIR=$BASE_DIR/configs
+RESULTS_DIR=$BASE_DIR/results
+mkdir -p $RESULTS_DIR
+
+FILES=($CONFIGS_DIR/run_*.json)
+
+FILE=${FILES[$SLURM_ARRAY_TASK_ID-1]}
+DEST_FILE=$RESULTS_DIR/$(basename $FILE)
+
+ml python/3.9.0
+ml py-pytorch/2.0.0_py39
+. $HOME/alo/.venv/bin/activate
+
+python run.py $FILE $DEST_FILE
+
