Lots of experiments, jacobian norm fix

alogcv/alo.py
@@ -54,12 +54,12 @@ class ALOBase(ABC):
         self.a_c = self._dloss_dy_hat / self._d2loss_dy_hat2
         self.c_b = self._d2loss_dy_hat2 / self._d2loss_dboth
 
-    def transform_jac(self, diag_jac: Tensor) -> Tensor:
+    def transform_jac_linop(self, jac: LinearOperator) -> LinearOperator:
         """Transform the Jacobian into a generic form.
 
-        For general losses, the Jacobian may not cover the range from 0 to 1.
+        For general losses, the Jacobian diagonals be constrained within [0, 1).
         This function transforms the Jacobian into a generic form that does
-        cover the range from 0 to 1 by eliminating loss-specific terms.
+        cover the range from 0 to 1 by eliminating some loss-specific terms.
 
         Parameters
         ----------
@@ -72,7 +72,7 @@ class ALOBase(ABC):
         Tensor
             The transformed diagonal of the Jacobian.
         """
-        return -diag_jac * self.c_b.expand_as(diag_jac)
+        return jac @ lo.DiagonalOperator(-self.c_b)
 
     def y_tilde_from_transformed_jac(self, transformed_jac: Tensor) -> Tensor:
         """Compute the corrected predictions using the transformed Jacobian.
@@ -107,7 +107,7 @@ class ALOBase(ABC):
             The corrected predictions.
         """
 
-        return self.y_tilde_from_transformed_jac(self.transform_jac(diag_jac))
+        return self.y_tilde_from_transformed_jac(-diag_jac * self.c_b)
 
     def joint_vars(self) -> Tuple[Tensor, Tensor]:
         """Return the true response and corrected predictions."""
@@ -216,7 +216,7 @@ class ALORandomized(RandomizedMixin, ALOBase):
             The random number generator.
         """
         super().__init__(loss_fun, y, y_hat)
-        self._jac = jac
+        self._jac = self.transform_jac_linop(jac)
 
         self._transformed_diag_jac_estims = None
         self.m = 0
@@ -249,7 +249,7 @@ class ALORandomized(RandomizedMixin, ALOBase):
         matvecs, Omega = self._get_matvecs(m - self.m)
 
         # update the diagonal Jacobian estimates
-        transformed_diag_jac_estims = self.transform_jac((matvecs * Omega).T).T
+        transformed_diag_jac_estims = matvecs * Omega
         if self._transformed_diag_jac_estims is None:
             self._transformed_diag_jac_estims = transformed_diag_jac_estims
         else:
@@ -331,17 +331,17 @@ class ALORandomized(RandomizedMixin, ALOBase):
             )
 
             # compute the risk estimate
-            risks[i, :] = (
-                risk(self._y, self.y_tilde_from_transformed_jac(diag_jac)).numpy()
-            )
+            risks[i, :] = risk(
+                self._y, self.y_tilde_from_transformed_jac(diag_jac)
+            ).numpy()
 
         cov = self.n * np.cov(risks)
         # return the constant term of the polynomial fit
         self._ms = ms
         self._risks = risks.sum(axis=1)
-        #coefs, self._res_m_to_risk_fit = utils.robust_poly_fit(
-            #1 / ms**power, self._risks, order
-        #)
+        # coefs, self._res_m_to_risk_fit = utils.robust_poly_fit(
+        # 1 / ms**power, self._risks, order
+        # )
         risk, stddev = utils.weighted_lstsq_fit(1 / ms**power, self._risks, order, cov)
         self.error_estimate = stddev
 
@@ -383,7 +383,7 @@ class GCV(RandomizedMixin, ALOBase):
         """
         super().__init__(loss_fun, y, y_hat)
 
-        self._jac = jac
+        self._jac = self.transform_jac_linop(jac)
         self.m = m
 
         if generator is None:
@@ -395,7 +395,7 @@ class GCV(RandomizedMixin, ALOBase):
 
         # apply Hutchinson's method to estimate the trace of the normalized Jacobian
         matvecs, Omega = self._get_matvecs(m)
-        transformed_diag_jac_estim = self.transform_jac((matvecs * Omega).T).mean(dim=0)
+        transformed_diag_jac_estim = (matvecs * Omega).mean(dim=1)
         tr_hat = transformed_diag_jac_estim.sum()
         tr_hat.clamp_(min=1e-12, max=self.n * (1 - 1e-12))
 

alogcv/models.py
@@ -5,8 +5,10 @@ from scipy import sparse
 from scipy.sparse.linalg import LinearOperator as ScipyLinearOperator, minres
 import torch
 
-from sklearn.linear_model import Lasso, LogisticRegression
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.exceptions import NotFittedError
+from sklearn.linear_model import Lasso, LogisticRegression
+
 
 import linops as lo
 from linops import LinearOperator
@@ -188,7 +190,7 @@ class SeparableRegularizerJacobian(LinearOperator):
             Z = torch.linalg.ldl_solve(self.LD, self.pivots, B)
         # sparse X case:
         else:
-            B = self.X_mask.T @ A.numpy()
+            B = self.X_mask.T @ A.detach().numpy()
             Z = minres(self.H, B)[0]
             need_squeeze = False
 
@@ -276,6 +278,46 @@ class LinearSeparableRegularizerJacobian(LinearOperator):
     @property
     def diag(self):
         return torch.diag(self @ torch.eye(self.shape[0]))
+
+
+class RandomForestRegressorJacobian(LinearOperator):
+
+    supports_operator_matrix = True
+
+    def __init__(self, forest, X, dtype, device="cpu"):
+
+        self._shape = (X.shape[0], X.shape[0])
+        self.dtype = dtype
+        self.device = device
+
+        J = np.zeros((X.shape[0], X.shape[0]))
+        for est in forest.estimators_:
+            tree = est.tree_
+            S = sparse.csr_array(
+                (
+                    np.ones(X.shape[0]),
+                    tree.apply(X.astype(np.float32)),
+                    np.arange(X.shape[0] + 1),
+                ),
+                shape=(X.shape[0], tree.node_count),
+            )
+            denom = S.sum(axis=0)[None, :]
+            # Avoid division by zero
+            denom[denom == 0] = 1
+            J += (S / denom) @ S.T
+        J /= len(forest.estimators_)
+
+        self.J = sparse.csr_array(J)
+
+    def _matmul_impl(self, A):
+        return torch.as_tensor(self.J @ A.detach().numpy())
+
+    def todense(self):
+        return self.J.toarray()
+
+    @property
+    def diag(self):
+        return torch.as_tensor(self.J.diagonal())
 
 
 class SeparableRegularizerMixin(ABC):
@@ -470,3 +512,28 @@ class LogisticModel(LinearMixin, SeparableRegularizerMixin, ALOModel):
     @staticmethod
     def loss_fun(y, y_hat):
         return torch.log(1 + torch.exp(-y * y_hat))
+
+
+class RandomForestRegressorModel(ALOModel):
+
+    def __init__(self, sklearn_rf_kwargs):
+        super().__init__()
+        self.sklearn_rf_kwargs = sklearn_rf_kwargs
+        self.sklearn_rf_kwargs["bootstrap"] = True
+        self.sklearn_rf_kwargs["oob_score"] = True
+
+    def get_new_model(self):
+        return RandomForestRegressor(**self.sklearn_rf_kwargs)
+
+    def _compute_jac(self, device=None):
+        return RandomForestRegressorJacobian(
+            self.model, self._X.detach().numpy(), self._X.dtype, self._X.device
+        )
+
+    def oob_prediction(self):
+        self._fitted_check()
+        return self.model.oob_prediction_
+
+    @staticmethod
+    def loss_fun(y, y_hat):
+        return (y - y_hat) ** 2 / 2

benchmarking/collect_results.py
@@ -432,27 +432,47 @@ def lasso_bks_convergence(results):
     results = extract_all_results(results, axes_keys)
     (seeds,) = results.axes
 
+    color_cycle = plt.rcParams["axes.prop_cycle"].by_key()["color"]
+
     ms = np.asarray(results.alo_m)
-    ms_recip = 1 / ms
-    ms_recip = np.concatenate([1 / ms[:-1], [0.0]])
+    # ms_recip = ms
+    # ms_recip = np.concatenate([1 / ms[:-1], [0.0]])
 
-    plt.plot(1 / ms, np.median(results.alo_bks_risks, axis=0).T, label="BKS-ALO")
-    plt.plot(1 / ms, np.median(results.alo_poly_risks, axis=0).T, "-.", label="RandALO")
+    plt.plot(
+        ms,
+        np.median(results.alo_bks_risks, axis=0).T,
+        "--",
+        color=color_cycle[0],
+        label="BKS-ALO",
+    )
+    plt.plot(
+        ms,
+        np.median(results.alo_poly_risks, axis=0).T,
+        color=color_cycle[1],
+        label="RandALO",
+    )
 
     plt.fill_between(
-        ms_recip,
+        ms,
         *np.percentile(results.alo_bks_risks, [25, 75], axis=0),
+        color=color_cycle[0],
         alpha=0.2,
     )
     plt.fill_between(
-        ms_recip,
+        ms,
         *np.percentile(results.alo_poly_risks, [25, 75], axis=0),
+        color=color_cycle[1],
         alpha=0.2,
     )
 
-    plt.axhline(np.median(results.test_risks), color="black", linestyle="--")
+    plt.axhline(
+        np.median(results.test_risks), color="black", linestyle=":", label="Test error"
+    )
     # fill between interquartile range of test risk
-    xlim = np.asarray([0, np.max(ms_recip)])
+    # xlim = np.asarray([0, np.max(ms_recip)])
+    # plt.xscale("log")
+    xlim = np.asarray([np.min(ms), np.max(ms)])
+    xlim = np.asarray([np.min(ms), 300])
     plt.fill_between(
         xlim,
         *np.percentile(results.test_risks, [25, 75])[:, None],
@@ -460,37 +480,18 @@ def lasso_bks_convergence(results):
         alpha=0.2,
     )
     ylim = plt.ylim()
-    aspect_ratio = (xlim[1] - xlim[0]) / (ylim[1] - ylim[0])
-
-    for angle in np.linspace(0, np.pi / 2, 9)[1:-1]:
-        plt.plot(
-            xlim,
-            np.median(results.alo_bks_risks[:, -1])
-            + np.tan(angle) * (xlim - xlim[0]) / aspect_ratio,
-            color="black",
-            linestyle=":",
-            alpha=0.2,
-        )
-
-    ms_filter = np.asarray([m for m in ms if m >= 25 and m <= 50])
-    i_ms = [results.alo_m.index(m) for m in ms_filter]
-
-    risks = np.median(results.alo_bks_risks[:, i_ms], axis=0)
-
-    plt.scatter(
-        1 / ms_filter,
-        np.median(results.alo_bks_risks[:, i_ms], axis=0),
-    )
 
     plt.xlim(xlim)
     plt.ylim(ylim)
 
-    plt.title("Convergence of Risk Estimate in $m$")
-    plt.xlabel("$1/m$")
+    plt.title("Convergence to ALO")
+    plt.xlabel("Number of matvecs $m$")
     plt.ylabel("Risk estimate")
     plt.legend()
 
-    plt.show()
+    plt.savefig(
+        os.path.join("figures", "lasso_bks_convergence.pdf"), bbox_inches="tight"
+    )
 
 
 def lasso_sweep(results):
@@ -507,105 +508,97 @@ def lasso_sweep(results):
     m = 50
     i_m = results.alo_m.index(m)
 
-    plt.plot(lamda0s, np.median(results.gen_risks, axis=1), label="Theoretical risk")
-    plt.plot(lamda0s, np.median(results.test_risks, axis=1), label="Test error")
-    plt.plot(lamda0s, np.median(results.cv_risks[..., i_k], axis=1), label=f"CV(K={k})")
-    plt.plot(
-        lamda0s,
-        np.median(results.alo_bks_risks[..., i_m], axis=1),
-        label=f"BKS-ALO(m={m})",
-    )
-    plt.plot(
-        lamda0s,
-        np.median(results.alo_poly_risks[..., i_m], axis=1),
-        label=f"RandALO(m={m})",
-    )
+    color_cycle = plt.rcParams["axes.prop_cycle"].by_key()["color"]
+    markers = ["o", "s", "D", "v", "^", ">", "<", "p", "h", "H", "d", "P", "X"]
 
-    plt.xscale("log")
-    plt.yscale("log")
-    plt.legend()
-
-    plt.show()
-
-
-def lasso_scaling_1():
-
-    # TODO: refactor!!!
-
-    results = load_results(os.path.join("lasso_scaling_1", "results"))
-
-    axes_keys = [
-        ["config", "data", "n_train"],
-        ["config", "method_kwargs", "lamda0"],
-        ["config", "seed"],
+    data = [
+        results.test_risks,
+        results.cv_risks[..., i_k],
+        results.alo_bks_risks[..., i_m],
+        results.alo_poly_risks[..., i_m],
+    ]
+    labels = [
+        "Test error",
+        f"CV(K={k})",
+        f"BKS-ALO(m={m})",
+        f"RandALO(m={m})",
     ]
 
-    results = extract_all_results(results, axes_keys)
-    ns, lamda0s, seeds = results.axes
+    fig, axes = plt.subplots(1, 2, figsize=(10, 4.5), dpi=300)
 
-    scaling_subplots(results)
+    for i, (label, color) in enumerate(zip(labels, color_cycle)):
+        # shift markevery by 1 to avoid overlapping markers
+        markevery = (i, len(data))
+        axes[0].errorbar(
+            lamda0s,
+            np.mean(data[i], axis=1),
+            yerr=np.std(data[i], axis=1),
+            label=label,
+            color=color,
+            marker=markers[i],
+            markevery=markevery,
+            errorevery=markevery,
+            capsize=2,
+        )
 
-    k = 5
-    m = 100
-    lamda0 = 1.0
-    ik = cv_k.index(k)
-    im = alo_m.index(m)
-    ilamda0 = lamda0s.index(lamda0)
+    axes[0].set_xscale("log")
+    axes[0].set_title("Risk")
+    axes[0].set_ylabel("Squared Error")
+    axes[0].set_xlabel("Regularization parameter $\\lambda$")
+    axes[0].legend()
 
-    test_rel = relative_error(test_risks, gen_risks)[:, ilamda0, :]
-    cv_rel = relative_error(cv_risks[..., ik], gen_risks)[:, ilamda0, :]
-    alo_exact_rel = relative_error(alo_exact_risks, gen_risks)[:, ilamda0, :]
-    alo_bks_rel = relative_error(alo_bks_risks[..., im], gen_risks)[:, ilamda0, :]
-    alo_poly_rel = relative_error(alo_poly_risks[..., im], gen_risks)[:, ilamda0, :]
+    data = [
+        results.full_train_times,
+        results.cv_times[..., i_k],
+        results.alo_bks_times[..., i_m],
+        results.alo_poly_times[..., i_m],
+    ]
+    labels = [
+        "Training",
+        f"CV(K={k})",
+        f"BKS-ALO(m={m})",
+        f"RandALO(m={m})",
+    ]
 
-    fig, axes = plt.subplots(1, 2, figsize=(10, 5))
+    for i, (label, color) in enumerate(zip(labels, color_cycle)):
+        # shift markevery by 1 to avoid overlapping markers
+        markevery = (i, len(data))
+        axes[1].errorbar(
+            lamda0s,
+            np.mean(data[i], axis=1),
+            yerr=np.std(data[i], axis=1),
+            label=label,
+            color=color,
+            marker=markers[i],
+            markevery=markevery,
+            errorevery=markevery,
+            capsize=2,
+        )
 
-    normalize_factor = 1 / np.median(cv_rel, axis=1)[:, None]
-    grouped_boxplot(
-        [
-            cv_rel * normalize_factor,
-            alo_exact_rel * normalize_factor,
-            alo_bks_rel * normalize_factor,
-            alo_poly_rel * normalize_factor,
-        ],
-        [f"n={n}" for n in ns],
-        [f"cv_{k}", "alo_exact", f"alo_{m}_bks", f"alo_{m}_poly"],
-        ax=axes[0],
-    )
-    axes[0].axhline(1, color="black", linestyle="--")
-    axes[0].set_title(f"Lasso Error Scaling for $\\lambda_0={lamda0}$")
-    axes[0].set_ylabel("Relative Estimation Error (normalized by CV median)")
-
-    # normalize_factor = 1 / np.median(cv_times[:, ilamda0, :, ik], axis=1)[:, None]
-    normalize_factor = 1 / np.median(full_train_times[:, ilamda0, :], axis=1)[:, None]
-    grouped_boxplot(
-        [
-            cv_times[:, ilamda0, :, ik] * normalize_factor,
-            alo_exact_times[:, ilamda0, :] * normalize_factor,
-            alo_bks_times[:, ilamda0, :, im] * normalize_factor,
-            alo_poly_times[:, ilamda0, :, im] * normalize_factor,
-        ],
-        [f"n={n}" for n in ns],
-        [f"cv_{k}", "alo_exact", f"alo_{m}_bks", f"alo_{m}_poly"],
-        ax=axes[1],
-    )
-    axes[1].axhline(1, color="black", linestyle="--")
-    solve_time = np.median(full_train_times[-1, ilamda0, :] * normalize_factor[-1, :])
-    # axes[1].axhline(solve_time, color="black", linestyle=":")
-    axes[1].set_title(f"Lasso Time Scaling for $\\lambda_0={lamda0}$")
-    axes[1].set_ylabel("Time (normalized by CV median)")
+    axes[1].set_xscale("log")
     axes[1].set_yscale("log")
+    axes[1].set_title("Time (s)")
+    axes[1].set_xlabel("Regularization parameter $\\lambda$")
+    axes[1].legend()
 
     plt.tight_layout()
-    plt.show()
+    plt.savefig(os.path.join("figures", "lasso_sweep.pdf"), bbox_inches="tight")
+
+
+def categorical_comp(results):
+    general_comp(results, "categorical_comp", k=5, m=100)
 
 
 def logistic_comp(results):
     general_comp(results, "logistic_comp", k=5, m=100)
 
 
-def categorical_comp(results):
-    general_comp(results, "categorical_comp", k=5, m=100)
+def multivariate_t_comp(results):
+    general_comp(results, "multivariate_t_comp", k=5, m=100)
+
+
+def random_forest_comp(results):
+    general_comp(results, "random_forest_comp", k=5, m=100)
 
 
 def general_comp(results, out_name, k=5, m=100):
@@ -619,7 +612,7 @@ def general_comp(results, out_name, k=5, m=100):
     i_k = results.cv_k.index(k)
     i_m = results.alo_m.index(m)
 
-    fig, axes = plt.subplots(1, 2, figsize=(5.5, 4), dpi=300)
+    fig, axes = plt.subplots(1, 2, figsize=(5, 4), dpi=300)
 
     grouped_boxplot(
         [
@@ -629,7 +622,7 @@ def general_comp(results, out_name, k=5, m=100):
             results.alo_poly_risks[..., i_m],
         ],
         [""],
-        ["Test error", "CV", "BKS-ALO", "RandALO"],
+        ["Test error", f"CV(K={k})", f"BKS-ALO(m={m})", f"RandALO(m={m})"],
         ax=axes[0],
     )
     axes[0].set_title("Risk")
@@ -643,7 +636,7 @@ def general_comp(results, out_name, k=5, m=100):
             results.alo_poly_times[..., i_m],
         ],
         [""],
-        ["Training", "CV", "BKS-ALO", "RandALO"],
+        ["Training", f"CV(K={k})", f"BKS-ALO(m={m})", f"RandALO(m={m})"],
         ax=axes[1],
     )
     axes[1].set_title("Time (s)")
@@ -811,10 +804,11 @@ def first_diff_scaling():
 collect_mapping = {
     "categorical_comp": categorical_comp,
     "lasso_bks_convergence": lasso_bks_convergence,
-    "lasso_scaling_1": lasso_scaling_1,
     "lasso_scaling_normal": lasso_scaling_normal,
     "lasso_sweep": lasso_sweep,
     "logistic_comp": logistic_comp,
+    "multivariate_t_comp": multivariate_t_comp,
+    "random_forest_comp": random_forest_comp,
     "first_diff_scaling_1": first_diff_scaling_1,
 }
 

benchmarking/run.py
@@ -16,7 +16,13 @@ from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from sklearn.pipeline import Pipeline
 
 from alogcv.alo import ALOExact, ALORandomized, GCV
-from alogcv.models import LinearMixin, LassoModel, FirstDifferenceModel, LogisticModel
+from alogcv.models import (
+    LinearMixin,
+    LassoModel,
+    FirstDifferenceModel,
+    LogisticModel,
+    RandomForestRegressorModel,
+)
 
 
 class Timer(object):
@@ -244,16 +250,53 @@ def get_data(data_config, rng):
             lambda beta_hat: (np.linalg.norm(beta - beta_hat) ** 2 + sigma**2) / 2
         )
 
+    elif data_config["src"] == "multivariate_t_sparse_awgn":
+        n_train, n_test, p, nu, s, sigma = extract_dict_keys(
+            data_config, ["n_train", "n_test", "p", "nu", "s", "sigma"]
+        )
+        X_train = rng.normal(size=(n_train, p)) * np.sqrt(
+            (nu - 2) / rng.chisquare(nu, size=n_train)[:, None]
+        )
+        X_test = rng.normal(size=(n_test, p)) * np.sqrt(
+            (nu - 2) / rng.chisquare(nu, size=n_test)[:, None]
+        )
+
+        beta = np.zeros(p)
+        beta[:s] = rng.normal(size=s) / np.sqrt(s)
+
+        y_train = X_train @ beta + rng.normal(scale=sigma, size=n_train)
+        y_test = X_test @ beta + rng.normal(scale=sigma, size=n_test)
+
+        gen_risk_linear = (
+            lambda beta_hat: (np.linalg.norm(beta - beta_hat) ** 2 + sigma**2) / 2
+        )
+
+    elif data_config["src"] == "categorical_sparse_awgn":
+        n_train, n_test, p, n_categories, s, sigma = extract_dict_keys(
+            data_config, ["n_train", "n_test", "p", "n_categories", "s", "sigma"]
+        )
+        X0_train = rng.integers(0, n_categories, size=(n_train, p))
+        onehot = OneHotEncoder()
+        X_train = onehot.fit_transform(X0_train)
+        X0_test = rng.integers(0, n_categories, size=(n_test, p))
+        X_test = onehot.transform(X0_test)
+        beta = np.zeros(n_categories * p)
+        nz_idx = rng.choice(p, s, replace=False)
+        beta[nz_idx] = rng.normal(size=s) * np.sqrt(n_categories / s)
+        y_train = X_train @ beta + rng.normal(scale=sigma, size=n_train)
+        y_test = X_test @ beta + rng.normal(scale=sigma, size=n_test)
+
+        gen_risk_linear = None
+
     elif data_config["src"] == "iid_normal_logistic_sparse":
         n_train, n_test, p, s, rho = extract_dict_keys(
             data_config, ["n_train", "n_test", "p", "s", "rho"]
         )
-        x_mean = rng.normal(size=(1, p))
-        X_train = rng.normal(size=(n_train, p)) + x_mean / np.sqrt(s) * 2
-        X_test = rng.normal(size=(n_test, p)) + x_mean / np.sqrt(s) * 2
+        X_train = rng.normal(size=(n_train, p))
+        X_test = rng.normal(size=(n_test, p))
         beta = np.zeros(p)
-        # beta[:s] = rng.normal(size=s) / np.sqrt(s)
-        beta[:s] = x_mean[0, :s] / np.sqrt(s)
+        beta[:s] = rng.normal(size=s) / np.sqrt(s)
+        # beta[:s] = x_mean[0, :s] / np.sqrt(s)
 
         prob = 1 / (1 + np.exp(-X_train @ beta * rho))
         y_train = (rng.uniform(size=n_train) < prob).astype(float) * 2 - 1
@@ -273,6 +316,28 @@ def get_data(data_config, rng):
         beta[:s] = rng.normal(size=s) / np.sqrt(s)
         y_train = X_train @ beta + rng.normal(scale=sigma, size=n_train)
         y_test = X_test @ beta + rng.normal(scale=sigma, size=n_test)
+
+        gen_risk_linear = (
+            lambda beta_hat: (np.linalg.norm(beta - beta_hat) ** 2 + sigma**2) / 2
+        )
+
+    if data_config["src"] == "iid_normal_sparse_poly_awgn":
+        n_train, n_test, p, ord, s, sigma = extract_dict_keys(
+            data_config, ["n_train", "n_test", "p", "ord", "s", "sigma"]
+        )
+        X_train = rng.normal(size=(n_train, p))
+        X_test = rng.normal(size=(n_test, p))
+
+        X_train_poly = np.zeros((n_train, s))
+        X_test_poly = np.zeros((n_test, s))
+        for i in range(s):
+            poly_factors = rng.integers(0, p, size=ord)
+            X_train_poly[:, i] = np.prod(X_train[:, poly_factors], axis=1)
+            X_test_poly[:, i] = np.prod(X_test[:, poly_factors], axis=1)
+
+        beta = rng.normal(size=s) / np.mean(np.linalg.norm(X_train_poly, axis=1))
+        y_train = X_train_poly @ beta + rng.normal(scale=sigma, size=n_train)
+        y_test = X_test_poly @ beta + rng.normal(scale=sigma, size=n_test)
 
         gen_risk_linear = (
             lambda beta_hat: (np.linalg.norm(beta - beta_hat) ** 2 + sigma**2) / 2
@@ -323,6 +388,9 @@ def model_lookup(config):
         elif method_kwargs["penalty"] == "l2":
             lamda = method_kwargs.pop("lamda0")
         return LogisticModel(lamda, sklearn_logistic_kwargs=method_kwargs)
+
+    if method == "random-forest":
+        return RandomForestRegressorModel(sklearn_rf_kwargs=method_kwargs)
 
 
 def risk_lookup(risk_name):
@@ -483,6 +551,10 @@ if __name__ == "__main__":
         )
         results["gcv_risk"] = gcv.eval_risk(risk_fun) / n
     results["gcv_time"] = timer.elapsed
+
+    if config["method"] == "random-forest":
+        log("Computing OOB risk...")
+        results["oob_risk"] = sum(risk_fun(y_train, model.oob_prediction())) / n
 
     # Save the results
     with open(args.results_file, "w") as f: