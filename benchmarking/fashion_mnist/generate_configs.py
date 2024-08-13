0c345659508e2d95d04bd65669ae73804a8badec
Daniel LeJeune <16905520+dlej@users.noreply.github.com>
2024-08-12 17:20:16 -0700

    update fashion MNIST exp

--- benchmarking/fashion_mnist/generate_configs.py
+++ benchmarking/fashion_mnist/generate_configs.py
@@ -8,13 +8,13 @@ with open("base_config.json") as f:
 
 os.makedirs("configs", exist_ok=True)
 
-for seed in range(1):
+for seed in range(10):
     config["seed"] = seed
 
-    for lamda in [1e-3, 3e-3, 1e-2, 3e-2, 1e-1, 3e-1, 1e0, 3e0, 1e1]:
+    for lamda in [1e-3, 3e-3, 1e-2, 3e-2, 1e-1, 3e-1, 1e0]:
         config["method_kwargs"]["lamda"] = lamda
 
-        for gamma in [1e-7, 3e-7, 1e-6, 3e-6, 1e-5, 3e-5, 1e-4, 3e-4, 1e-3]:
+        for gamma in [1e-8, 3e-8, 1e-7, 3e-7, 1e-6, 3e-6, 1e-5]:
             config["method_kwargs"]["kernel_fun_kwargs"]["gamma"] = gamma
 
             id = hashlib.sha256(json.dumps(config).encode()).hexdigest()[:8]
