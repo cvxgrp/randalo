7b937b73275f63f13eab2b234b8dc1ea1660be48
Parth Nobel <parthnobel@berkeley.edu>
2026-09-19 05:01:53 -0700

    Repair CVXPY model conversion (#6)

--- test/test_reductions.py
+++ test/test_reductions.py
@@ -1,10 +1,8 @@
 import unittest
 
-import linops as lo
 import cvxpy as cp
 import numpy as np
 import torch
-from cvxpylayers.torch import CvxpyLayer
 
 from randalo import modeling_layer as ml, reductions
 
@@ -19,29 +17,55 @@ class TestReductions(unittest.TestCase):
         self.y = self.X[:, 0] + 0.01 * self.rng.standard_normal(self.n)
 
     def test_transform_to_cvxpy_and_test_jacobian(self):
-        # TODO fix segemntation fault?
-        return
-
         b = cp.Variable(3 * self.n)
         y = cp.Parameter(self.n)
         prob = reductions.transform_model_to_cvxpy(
-            self.loss, self.regularizer, self.X, y, b
+            self.loss,
+            self.regularizer,
+            self.X,
+            y,
+            b,
         )
-        self.layer = CvxpyLayer(prob, parameters=[y], variables=[b])
-
-        y_torch = torch.tensor(self.y, requires_grad=True)
-        J = reductions.Jacobian(
-            self.y, self.X, lambda: b.value, self.loss, self.regularizer
+        jacobian = reductions.Jacobian(
+            self.y,
+            self.X,
+            lambda: b.value,
+            self.loss,
+            self.regularizer,
+            inverse_method="cholesky",
         )
+        _, generated_jacobian = reductions.gen_cvxpy_jacobian(
+            self.loss,
+            self.regularizer,
+            self.X,
+            b,
+            self.y,
+            inversion_method="cholesky",
+        )
+        self.assertEqual(generated_jacobian.inverse_method, "cholesky")
 
+        solve_options = {
+            "solver": "CLARABEL",
+            "tol_gap_abs": 1e-10,
+            "tol_gap_rel": 1e-10,
+            "tol_feas": 1e-10,
+        }
         y.value = self.y
-        prob.solve()
-        y_hat_torch = torch.from_numpy(self.X) @ self.layer(y_torch)[0]
+        prob.solve(**solve_options)
+        direction = self.rng.standard_normal(self.n)
+        actual = jacobian @ direction
 
-        z = self.rng.standard_normal(self.n)
-        Jz = J @ z
-        (y_hat_torch @ torch.from_numpy(z)).backward()
-        assert torch.allclose(Jz, y_torch.grad.to(torch.float32), atol=1e-4, rtol=1e-2)
+        epsilon = 1e-4
+        predictions = []
+        for sign in (-1, 1):
+            y.value = self.y + sign * epsilon * direction
+            prob.solve(warm_start=True, **solve_options)
+            predictions.append(self.X @ b.value)
+        expected = torch.as_tensor(
+            (predictions[1] - predictions[0]) / (2 * epsilon),
+            dtype=torch.float32,
+        )
+        self.assertTrue(torch.allclose(actual, expected, atol=1e-3, rtol=1e-2))
 
 
 if __name__ == "__main__":
